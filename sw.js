739f662139685c6cde4a4c9dbab765ef3b192d34
Yahia <yahiaafouad2016@gmail.com>
2025-10-22 21:05:00 +0300

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,17 @@
+const cacheName = 'lightning-cache-v1';
+const filesToCache = [
+  '/',
+  '/index.html'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(cacheName).then(cache => cache.addAll(filesToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
+});
