61f3d264e3d8747fc647c2f9be7995f07c619c58
Yahia <yahiaafouad2016@gmail.com>
2025-10-23 15:18:38 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,23 +1,18 @@
-
-const cacheName = 'lightning-cache-v8';
+const cacheName = 'lightning-cache-v9';
 const filesToCache = [
-  '/lightning-games/',
-  '/lightning-games/index.html',
-  '/lightning-games/snake.html',
-  '/lightning-games/tetris.html'
+  './',
+  './index.html',
+  './snake.html',
+  './tetris.html'
 ];
 
-// Install and cache files
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(cacheName).then(cache => {
-      return cache.addAll(filesToCache);
-    })
+    caches.open(cacheName).then(cache => cache.addAll(filesToCache))
   );
   self.skipWaiting();
 });
 
-// Serve cached files
 self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request).then(response => response || fetch(event.request))
